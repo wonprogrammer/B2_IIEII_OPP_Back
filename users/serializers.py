d2411936629fddf645de077425113a1518bd176f
auberr <auberr@daum.net>
2022-11-22 17:12:44 +0900

    [add] 회원가입, 로그인 최소 기본 기능

---
+++ users/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from users.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+    def create(self, validated_data):   # 유저 생성시 set_password
+        user = super().create(validated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):   # email로 로그인하는 jwt 커스터마이징 하기위한 serializer
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+
+        return token
