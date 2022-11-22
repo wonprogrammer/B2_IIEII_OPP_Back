acf793a03ae7840c3537c50ce0e0ba52bdf99ec0
leehyewon <tnddl2345@gmail.com>
2022-11-22 17:18:35 +0900

    init project

--- opp/urls.py
+++ opp/urls.py
@@ -1,9 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("user/",include("users.urls")),
+    path("users/",include("users.urls")),
     path("op/",include("oilpainting.urls")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
