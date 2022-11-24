d1514f348f6b5d7e672c388336b05d85d5d7dbad
uniqquej <cjy4477@gmail.com>
2022-11-24 17:07:57 +0900

    [add] 좋아요 기능 추가

--- oilpainting/serializers.py
+++ oilpainting/serializers.py
@@ -18,13 +18,13 @@ class ArticleCreateSerializer(serializers.ModelSerializer):
         fields = ['title','content','img']
 
 class ArticleListSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
+    article_user = serializers.SerializerMethodField()
 
     # 1. list를 불러올때 좋아요 갯수도 불러오고싶을때! + commnets_count도 동일!
     likes_count = serializers.SerializerMethodField()
 
     # 여기서 정의된 user의 email이 위에 user값에 들어가게 된다
-    def get_user(self, obj):
+    def get_article_user(self, obj):
         return obj.article_user.username
 
     # 2. 위에서 좋아요 수 정의 해주고, 여기서 갯수 받아오는 함수 정의
@@ -43,9 +43,12 @@ class ImageSerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer): # main get
     img = ImageSerializer()
+    likes_count = serializers.SerializerMethodField()
+    def get_likes_count(self, obj):
+        return obj.likes.count()
     class Meta:
         model = Article
-        fields = ['id','img','article_user','likes']
+        fields = ['id','img','article_user','likes','likes_count']
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
     img = ImageSerializer()
--- oilpainting/urls.py
+++ oilpainting/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
         path('imgtoop/', views.ImageUploadview.as_view(), name='image_upload_view'),
         path('imgtoop/2/',views.ArticleView.as_view(), name = 'article_view'),
         path('<int:article_id>/detail/', views.ArticleDetailView.as_view(), name = 'detail_view'),
+        path('<int:article_id>/likes/', views.LikeView.as_view(), name = 'like_view'),
 ]
